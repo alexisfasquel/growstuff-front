82a43ce8158ddb68f1aa9080fc1eb5a31f96a3b7
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-03-30 17:31:37 +0200

    Ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -307,6 +307,9 @@ module.exports = function (grunt) {
                       console.log(match);
                        return match.slice(0,1) + "http://alexisfasquel.github.io/growstuff-front/" + match.slice(1);
                     }
+                  }, {
+                    from: "href=\"/\"",
+                    to: "href=\"http://alexisfasquel.github.io/growstuff-front/\""
                   }]
             }
       }
